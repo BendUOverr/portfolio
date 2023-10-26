492eb605a4eb2711caaa4df73415a81225985c55
Luka <138794790+BendUOverr@users.noreply.github.com>
2023-10-26 21:04:15 +0200

    third item

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Trafalgar from "../assets/trafalgar.png";
 import crypto from "../assets/crypto.png";
+import beaches from "../assets/BeachesApp.png";
 
 export const Work = () => {
   return (
@@ -19,7 +20,10 @@ export const Work = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* First Item */}
           <div
-            style={{ backgroundImage: `url(${Trafalgar})`, backgroundSize: 'contain' }}
+            style={{
+              backgroundImage: `url(${Trafalgar})`,
+              backgroundSize: "contain",
+            }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-gray-200"
           >
             <div className="opacity-0 group-hover:opacity-100">
@@ -43,12 +47,15 @@ export const Work = () => {
                     Code
                   </button>
                 </a>
-              </div> 
+              </div>
             </div>
           </div>
           {/* second item */}
           <div
-            style={{ backgroundImage: `url(${crypto})`, backgroundSize: 'contain' }}
+            style={{
+              backgroundImage: `url(${crypto})`,
+              backgroundSize: "contain",
+            }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-gray-700"
           >
             <div className="opacity-0 group-hover:opacity-100">
@@ -58,6 +65,38 @@ export const Work = () => {
               <div className="pt-8 text-center">
                 <a
                   href="https://lambent-melba-d530ad.netlify.app/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 p-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/BendUOverr/CryptoSci"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 p-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* third item */}
+          <div
+            style={{
+              backgroundImage: `url(${beaches})`,
+              backgroundSize: "contain",
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-gray-700"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://bright-narwhal-ba482a.netlify.app/"
                   target="_blank"
                 >
                   <button className="text-center rounded-lg px-4 p-3 m-2 bg-white text-gray-700 font-bold text-lg">
